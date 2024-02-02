8f55587272e49f56df7290ab52480955e0d45754
DevMdAsif <mdasifullah456@gmail.com>
2024-02-02 16:51:19 +0600

    fetching data updated

--- src/hooks/FetchData/useFetchData.jsx
+++ src/hooks/FetchData/useFetchData.jsx
@@ -4,13 +4,13 @@ import { useEffect, useState } from "react";
 function useFetchData(url) {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState("");
-  const [foods, setFoods] = useState([]);
+  const [data, setdata] = useState([]);
   useEffect(() => {
     (async () => {
       try {
         const response = await axios.get(url);
         setLoading(false);
-        setFoods(response.data);
+        setdata(response.data);
       } catch (error) {
         setLoading(false);
         setError(error.message);
@@ -19,7 +19,7 @@ function useFetchData(url) {
 
     return () => {};
   }, [url]);
-  return { error, loading, foods };
+  return { error, loading, data };
 }
 
 export default useFetchData;
