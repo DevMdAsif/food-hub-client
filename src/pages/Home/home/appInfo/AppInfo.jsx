d0e1f0c51d312fd026fc0a71199b09e57c7145ce
DevMdAsif <mdasifullah456@gmail.com>
2024-02-03 11:01:47 +0600

    updated App-info

--- src/pages/Home/home/appInfo/AppInfo.jsx
+++ src/pages/Home/home/appInfo/AppInfo.jsx
@@ -2,7 +2,7 @@ import appImage from "../../../../assets/images/HomeImg/mockup-Lbl8JI0l.png";
 import Banner from "../../../../component/banner/Banner";
 function AppInfo() {
   return (
-    <div className="h-full bg-[#020617] p-3 md:p-7">
+    <div className="h-full bg-[#020617] p-3 pb-10 md:pb-14 md:p-7">
       <div className="bg-[#1b1218] p-5 pb-0 rounded-2xl lg:grid lg:grid-cols-2 lg:p-20 lg:pb-0">
         <div className="lg:mt-20 space-y-5">
           <Banner header="Download App" title="Get Started With Us Today!" />
