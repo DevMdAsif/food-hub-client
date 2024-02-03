269e5aa4beb7494427af1d57cbdc21dd7d4e69d0
DevMdAsif <mdasifullah456@gmail.com>
2024-02-03 12:59:11 +0600

    footer section added

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../pages/shared/navbar/navbar";
+import Footer from "../pages/shared/footer/Footer";
 
 function Main() {
   return (
     <div className="max-w-8xl">
       <Navbar />
       <Outlet />
+      <Footer />
     </div>
   );
 }
