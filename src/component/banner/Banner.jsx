b231933e4a160503985de378e4d5948046dfdd11
DevMdAsif <mdasifullah456@gmail.com>
2024-02-06 13:46:18 +0600

    banner updated

--- src/component/banner/Banner.jsx
+++ src/component/banner/Banner.jsx
@@ -1,11 +1,10 @@
 function Banner({ header, title }) {
+  const condition = header === "Check it out";
   return (
-    <div
-      className={`space-y-4 ${header === "Check it out" ? "text-center" : ""}`}
-    >
+    <div className={`space-y-4 ${condition ? "text-center" : ""}`}>
       <div
         className={`bg-[#352018] w-32  rounded-xl text-[#e77b1f] ${
-          header === "Check it out" ? "mx-auto" : ""
+          condition ? "mx-auto" : ""
         } `}
       >
         <h3 className="text-center">{header}</h3>
